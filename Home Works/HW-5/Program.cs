89ef107bbf51a0e42be9569a0db51d6b4e660daf
Igor <igorbobrovich@gmail.com>
2023-10-12 11:13:42 +0300

    добавлены ДЗ №5.6

---
+++ Home Works/HW-5/Program.cs
@@ -0,0 +1,239 @@
+﻿// Напишите программу, которая подсчитывает количество четных элементов в массиве целых положительных трехзначных чисел и выводит результат на экран.
+// Программа должна иметь метод CountEvenElements, который принимает массив целых положительных трехзначных чисел и возвращает количество четных элементов в массиве.
+// Программа должна иметь метод PrintArray, который выводит элементы массива на экран.
+// При выводе элементы массива должны быть разделены символом табуляции "\t".
+// Если аргументы командной строки не переданы, программа использует массив по умолчанию, который содержит следующие значения: {100, 102, 105, 166, 283, 764, 300, 499, 133}.
+
+// Массив:
+// 124     378     593     821     456 
+// Количество четных элементов: 3
+
+// ﻿using System;
+
+// public class Answer
+// {
+//   public static int CountEvenElements(int[] array)
+//   {
+//     // Введите свое решение ниже
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//       if (array[i] % 2 == 0)
+//         count += 1;
+//     }
+//     return count;
+//   }
+
+//   public static void PrintArray(int[] array)
+//   {
+//     // Введите свое решение ниже
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//       Console.Write($"{array[i]}\t");
+//     }
+//     Console.WriteLine();
+//   }
+// Не удаляйте и не меняйте метод Main! 
+    // public static void Main(string[] args)
+    // {
+        // int[] array;
+        // if (args.Length == 0)
+        // {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+        //     array = new int[] { 100, 102, 105, 166, 283, 764, 300, 499, 133 };
+        // }
+        // else
+        // {
+        //     string[] argStrings = args[0].Split(", ");
+        //     array = new int[argStrings.Length];
+        //     for (int i = 0; i < argStrings.Length; i++)
+        //     {
+        //         if (int.TryParse(argStrings[i], out int number))
+        //         {
+        //             array[i] = number;
+        //         }
+        //         else
+        //         {
+        //             Console.WriteLine($"Ошибка при парсинге аргумента {argStrings[i]}.");
+        //             return;
+        //         }
+        //     }
+        // }
+
+        // Не удаляйте строки ниже
+//         Console.WriteLine("Массив:");
+//         PrintArray(array);
+//         int evenCount = CountEvenElements(array);
+//         Console.WriteLine($"Количество четных элементов: {evenCount}");
+//     }
+// }
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+// Напишите программу, которая находит сумму элементов с нечетными индексами в одномерном массиве целых чисел и выводит результат на экран.
+// Программа должна иметь метод SumOddElements, который принимает массив целых чисел и возвращает сумму элементов с нечетными индексами в массиве.
+// Программа должна иметь метод PrintArray, который выводит элементы массива на экран.
+// При выводе элементы массива должны быть разделены символом табуляции "\t".
+// Аргументы, передаваемые в метод/функцию:
+
+// 18      76      11  
+// Сумма нечетных элементов: 76
+
+
+// using System;
+
+// public class Answer
+// {
+//     public static int SumOddElements(int[] array)
+//     {
+// // Введите свое решение ниже
+// int count = 0;
+//         for (int i = 0; i < array.Length; i++)
+//         {
+//             if (i % 2 > 0)
+//                 count += array[i];
+//         }
+//         return count;
+
+//     }
+
+//     public static void PrintArray(int[] array)
+//     {
+//          // Введите свое решение ниже
+//         for (int i = 0; i < array.Length; i++)
+//         {
+//             Console.Write($"{array[i]}\t");
+//         }
+//         Console.WriteLine();
+
+//     }
+// // Не удаляйте и не меняйте метод Main! 
+//     public static void Main(string[] args)
+//     {
+//         int[] array;
+//         if (args.Length == 0)
+//         {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             array = new int[] { 12, 64, 28, 93, 35, 47, 6, 72, 58, 21 };
+//         }
+//         else
+//         {
+//             string[] argStrings = args[0].Split(", ");
+//             array = new int[argStrings.Length];
+//             for (int i = 0; i < argStrings.Length; i++)
+//             {
+//                 if (int.TryParse(argStrings[i], out int number))
+//                 {
+//                     array[i] = number;
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine($"Ошибка при парсинге аргумента {argStrings[i]}.");
+//                     return;
+//                 }
+//             }
+//         }
+
+//         // Не удаляйте строки ниже
+//         PrintArray(array);
+//         int sumOdd = SumOddElements(array);
+//         Console.WriteLine($"Сумма нечетных элементов: {sumOdd}");
+//     }
+// }
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+// Напишите програНапишите программу для работы с массивом вещественных чисел.
+// Реализуйте класс ArrayOperations, который содержит следующие статические методы:
+// FindMax(double[] array): Метод принимает на вход массив вещественных чисел array и возвращает максимальное число из массива.
+// FindMin(double[] array): Метод принимает на вход массив вещественных чисел array и возвращает минимальное число из массива.
+// CalcDifferenceBetweenMaxMin(double[] array): Метод принимает на вход массив вещественных чисел array и возвращает разницу между максимальным и минимальным числами в массиве.
+// PrintArray(double[] array): Метод для вывода массива на экран. Он принимает на вход массив array и выводит его элементы с двумя знаками после запятой через табуляцию.
+
+// Массив:
+// 3.17    8.94    2.36    5.72    0.85    
+// Разность между максимальным и минимальным элементом = 8.09
+
+
+// public class Answer
+// {
+//     public static double FindMax(double[] array)
+//     {     // Введите свое решение ниже
+//     double max = array[0];
+//     for (int i = 1; i < array.Length; i++)
+//     {
+//       if (max < array[i])
+//         max = array[i];
+//     }
+//     return max;
+//     }
+
+//     public static double FindMin(double[] array)
+//     {     // Введите свое решение ниже
+//     double min = array[0];
+//     for (int i = 1; i < array.Length; i++)
+//     {
+//       if (min > array[i])
+//         min = array[i];
+//     }
+//     return min;
+//     }
+
+//     public static double CalcDifferenceBetweenMaxMin(double[] array)
+//     {// Введите свое решение ниже
+//     double min = FindMin(array);
+//     double max = FindMax(array);
+//     return max - min;
+//    }
+
+//     public static void PrintArray(double[] array)
+//     {// Введите свое решение ниже
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//       Console.Write($"{array[i]}\t");
+//     }
+//     Console.WriteLine();
+
+//     }
+//  // Не удаляйте и не меняйте метод Main! 
+
+
+//     public static void Main(string[] args)
+//     {
+//         double[] array;
+//         if (args.Length == 0)
+//         {
+//             array = new double[] { 3, 7.4, 22.3, 2, 78 };
+//         }
+//         else
+//         {
+//             // Иначе, парсим аргументы в массив чисел
+//             string[] argStrings = args[0].Split(", ");
+//             array = new double[argStrings.Length];
+//             for (int i = 0; i < argStrings.Length; i++)
+//             {
+//                 if (double.TryParse(argStrings[i], out double number))
+//                 {
+//                     array[i] = number;
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine($"Ошибка при парсинге аргумента {argStrings[i]}.");
+//                     return;
+//                 }
+//             }
+//         }
+
+//         Console.WriteLine("Массив:");
+//         PrintArray(array);
+//         double diff = CalcDifferenceBetweenMaxMin(array);
+//         Console.WriteLine($"Разность между максимальным и минимальным элементом = {diff:f2}");
+//     }
+// }
+
+
+
---
+++ Home Works/HW-6/Program.cs
@@ -0,0 +1,86 @@
+﻿﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+//
+
+// int countPositive(int[] array)
+// {
+//   int count = 0;
+//   for (int i = 0; i < array.Length; i++)
+//   {
+//     if (array[i] > 0)
+//       count++;
+//   }
+//   return count;
+// }
+
+// int[] inputInt(int M)
+// {
+//   int[] array = new int[M];
+//   for (int i = 0; i < M; i++)
+//   {
+//     Console.Write("Введите целое число: ");
+//     array[i] = int.Parse(Console.ReadLine());
+//   }
+//   return array;
+// }
+
+// Console.Write("Введите кол-во целых чисел: ");
+// int M = int.Parse(Console.ReadLine());
+// int[] array = inputInt(M);
+// Console.Write($"Ввели {countPositive(array)} положительных чисел");
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+//
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// double X(double k1, double k2, double b1, double b2)
+// {
+//     return (b2 - b1) / (k1 - k2);
+// }
+
+// double Y(double x, double k1, double b1)
+// {
+//     return k1 * x + b1;
+// }
+
+// double inputDouble(string promt)
+// {
+//     Console.Write(promt);
+//     return Convert.ToDouble(Console.ReadLine());
+// }
+
+// bool ValidateLines(double k1, double b1, double k2, double b2)
+// {
+//     if (k1 == k2)
+//     {
+//         if (b1 == b2)
+//             Console.WriteLine("Прямые совдадают");
+//         else
+//             Console.WriteLine("Прямые параллельны");
+
+//         return false;
+//     }
+//     return true;
+// }
+
+// Console.WriteLine("Для уравнений вида y=k*x+b");
+// double k1 = inputDouble("Введите к1: ");
+// double b1 = inputDouble("Введите b1: ");
+// double k2 = inputDouble("Введите к2: ");
+// double b2 = inputDouble("Введите b2: ");
+// if (ValidateLines(k1, b1, k2, b2))
+// {
+//     double x = X(k1, k2, b1, b2);
+//     double y = Y(x, k1, b1);
+//     Console.WriteLine($"Для уравнений y={k1}*x+{b1} и y={k2}*x+{b2}");
+//     Console.WriteLine($"Общая точка имеет координаты ({x}; {y})");
+// }
